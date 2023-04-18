85c1a9bd6a3d59004ee557b7d0a6d361d0ca7f49
siwon99 <kimsiwon0707@gmail.com>
2023-04-19 00:11:53 +0900

    JS : Upgrade performance

--- main.js
+++ main.js
@@ -22,32 +22,20 @@ function onAdd() {
   input.focus();
 }
 
+let id = 0; //UUID
 function createItem(text) {
   const itemRow = document.createElement('li');
   itemRow.setAttribute('class','item__row');
-
-  const item = document.createElement('div');
-  item.setAttribute('class','item');
-
-  const name = document.createElement('span');
-  name.setAttribute('class', 'item__name');
-  name.innerText = text;
-
-  const deleteBtn = document.createElement('button');
-  deleteBtn.setAttribute('class', 'item__delete');
-  deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
-  deleteBtn.addEventListener('click', () => {
-    items.removeChild(itemRow);
-  });
-  
-  const itemDivider = document.createElement('div');
-  itemDivider.setAttribute('class', 'item__divider');
-
-  item.appendChild(name);
-  item.appendChild(deleteBtn);
-
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
+  itemRow.setAttribute('data-id', id);
+  itemRow.innerHTML = `
+        <div class="item">
+          <span class="item__name">${text}</span>
+          <button class="item__delete">
+            <i class="fa-solid fa-trash-can" data-id=${id}></i>
+          </button>
+        </div>
+        <div class="item__divider"></div>`;
+  id++;
   return itemRow;
 }
 
@@ -62,3 +50,12 @@ input.addEventListener('keypress', (event) => {
     onAdd();
   }
 });
+
+items.addEventListener('click', event => {
+  const id = event.target.dataset.id;
+  if(id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
+
+  }
+})
